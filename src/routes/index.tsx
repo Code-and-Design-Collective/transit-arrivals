bd14918912b85c56858851364455e6f902f7407d
Ray Luna <ray@hello-upswell.com>
2023-07-10 17:09:22 -0700

    add api query

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,20 @@
 import { component$ } from "@builder.io/qwik";
+import { server$ } from "@builder.io/qwik-city";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import axios from "axios";
+
+const apiBaseUrl = import.meta.env.VITE_TRIMET_API_URL;
+const appId = import.meta.env.VITE_TRIMET_APP_ID;
+
+const fetchArrivals = server$(async () => {
+  const query = `${apiBaseUrl}/arrivals?locIDs=6849,6850&appID=${appId}`;
+  const data = await axios.get(query);
+  console.log(data)
+  return data;
+});
+
+fetchArrivals();
 export default component$(() => {
   return (
     <>
@@ -18,7 +32,7 @@ export const head: DocumentHead = {
   meta: [
     {
       name: "description",
-      content: "Transit arrivals app home npage",
+      content: "Transit arrivals app home page",
     },
   ],
 };
