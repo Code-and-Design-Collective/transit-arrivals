a98a0b16d3f1319f84723b0d499789842dbe171a
Ray Luna <leon.luna.ray@gmail.com>
2023-07-10 22:35:31 -0700

    clear ts errors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ import axios from 'axios';
 import { convertToMinutes } from '~/utils/date';
 
 import type { DocumentHead } from '@builder.io/qwik-city';
-import type { ArrivalsResultSet } from '~/interfaces/Arrivals';
+import type { StopArrivals } from '~/interfaces/Arrivals';
 
 const apiBaseUrl = import.meta.env.VITE_TRIMET_API_URL;
 const appId = import.meta.env.VITE_TRIMET_APP_ID;
@@ -18,21 +18,19 @@ const appId = import.meta.env.VITE_TRIMET_APP_ID;
 export default component$(() => {
   const stopID = useSignal('10764');
 
-  const arrivalsResource = useResource$<ArrivalsResultSet>(
-    async ({ track }) => {
-      track(() => stopID.value);
+  const arrivalsResource = useResource$<StopArrivals>(async ({ track }) => {
+    track(() => stopID.value);
 
-      try {
-        const query = `${apiBaseUrl}/arrivals?locIDs=${stopID.value},&appID=${appId}`;
-        const { data } = await axios.get(query);
-        // console.log(data.resultSet);
-        return data.resultSet;
-      } catch (error) {
-        console.error('Error occurred while fetching arrivals:', error);
-        throw error;
-      }
+    try {
+      const query = `${apiBaseUrl}/arrivals?locIDs=${stopID.value},&appID=${appId}`;
+      const { data } = await axios.get(query);
+      // console.log(data.resultSet);
+      return data.resultSet;
+    } catch (error) {
+      console.error('Error occurred while fetching arrivals:', error);
+      throw error;
     }
-  );
+  });
 
   return (
     <>
@@ -46,7 +44,7 @@ export default component$(() => {
                   <h2>{arrivals.location[0]?.desc}</h2>
                   <ul class='flex flex-col gap-y-[1rem] justify-center items-center text-center'>
                     {arrivals?.arrival.map((arrival, i) => (
-                      <li key={arrival.id}>{(arrival.estimated)}</li>
+                      <li key={arrival.id}>{arrival.estimated}</li>
                     ))}
                   </ul>
                 </div>
