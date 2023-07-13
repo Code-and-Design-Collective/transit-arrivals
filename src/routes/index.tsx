fc3a5ed65ec3954d7583b1dc85f80df9ff27ccd6
Ray Luna <ray@hello-upswell.com>
2023-07-13 15:31:43 -0700

    update times

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ import {
 } from "@builder.io/qwik";
 import axios from "axios";
 
-// import { convertToMinutes } from '~/utils/date';
+import { getArrivalTimeMinutes } from "~/utils/date";
 
 import type { DocumentHead } from "@builder.io/qwik-city";
 import type { StopArrivals } from "~/interfaces/Arrivals";
@@ -17,6 +17,10 @@ const appId = import.meta.env.VITE_TRIMET_APP_ID;
 export default component$(() => {
   const stopID = useSignal("10764");
 
+  const getBgColor = (hex: string): string => {
+    return `bg-[#${hex}]`;
+  };
+
   const arrivalsResource = useResource$<StopArrivals>(async ({ track }) => {
     track(() => stopID.value);
 
@@ -24,19 +28,14 @@ export default component$(() => {
       const query = `${apiBaseUrl}/arrivals?locIDs=${stopID.value},&appID=${appId}`;
       const { data } = await axios.get(query);
       // TODO move to util function
-      const getArrivalTimeMinutes = (
-        arrival: number,
-        queryTime: number
-      ): number => {
-        const differenceMillis = arrival - queryTime;
-        const differenceMinutes = Math.floor(differenceMillis / (1000 * 60));
 
-        return differenceMinutes;
-      };
       console.log(
-        getArrivalTimeMinutes(data.resultSet?.arrival[0]?.estimated, data.resultSet?.queryTime)
+        getArrivalTimeMinutes(
+          data.resultSet?.arrival[0]?.estimated,
+          data.resultSet?.queryTime
+        )
       );
-
+      console.log(data.resultSet);
       return data.resultSet;
     } catch (error) {
       console.error("Error occurred while fetching arrivals:", error);
@@ -55,7 +54,20 @@ export default component$(() => {
                 <div class="flex flex-col gap-y-[1.5rem]">
                   <h2>{arrivals.location[0]?.desc}</h2>
                   <ul class="flex flex-col gap-y-[1rem] justify-center items-center text-center">
-                    {}
+                    {arrivals.arrival.map((item, index) => (
+                      <li
+                        key={index}
+                        class={[
+                          getBgColor(item.routeColor),
+                        ]}
+                      >
+                        {getArrivalTimeMinutes(
+                          item.estimated,
+                          arrivals.queryTime
+                        )}{" "}
+                        Min
+                      </li>
+                    ))}
                   </ul>
                 </div>
               );
