b2e3a4d7ecfe823e8a561ddf31398de93454221d
Ray Luna <ray@hello-upswell.com>
2023-07-13 11:14:12 -0700

    add helper fuction for arrival time minutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,19 +3,19 @@ import {
   Resource,
   useResource$,
   useSignal,
-} from '@builder.io/qwik';
-import axios from 'axios';
+} from "@builder.io/qwik";
+import axios from "axios";
 
 // import { convertToMinutes } from '~/utils/date';
 
-import type { DocumentHead } from '@builder.io/qwik-city';
-import type { StopArrivals } from '~/interfaces/Arrivals';
+import type { DocumentHead } from "@builder.io/qwik-city";
+import type { StopArrivals } from "~/interfaces/Arrivals";
 
 const apiBaseUrl = import.meta.env.VITE_TRIMET_API_URL;
 const appId = import.meta.env.VITE_TRIMET_APP_ID;
 
 export default component$(() => {
-  const stopID = useSignal('10764');
+  const stopID = useSignal("10764");
 
   const arrivalsResource = useResource$<StopArrivals>(async ({ track }) => {
     track(() => stopID.value);
@@ -23,28 +23,39 @@ export default component$(() => {
     try {
       const query = `${apiBaseUrl}/arrivals?locIDs=${stopID.value},&appID=${appId}`;
       const { data } = await axios.get(query);
-      // console.log(data.resultSet);
+      // TODO move to util function
+      const getArrivalTimeMinutes = (
+        arrival: number,
+        queryTime: number
+      ): number => {
+        const differenceMillis = arrival - queryTime;
+        const differenceMinutes = Math.floor(differenceMillis / (1000 * 60));
+
+        return differenceMinutes;
+      };
+      console.log(
+        getArrivalTimeMinutes(data.resultSet?.arrival[0]?.estimated, data.resultSet?.queryTime)
+      );
+
       return data.resultSet;
     } catch (error) {
-      console.error('Error occurred while fetching arrivals:', error);
+      console.error("Error occurred while fetching arrivals:", error);
       throw error;
     }
   });
 
   return (
     <>
-      <div id='homepage'>
-        <div class='flex justify-center'>
+      <div id="homepage">
+        <div class="flex justify-center">
           <Resource
             value={arrivalsResource}
             onResolved={(arrivals) => {
               return (
-                <div class='flex flex-col gap-y-[1.5rem]'>
+                <div class="flex flex-col gap-y-[1.5rem]">
                   <h2>{arrivals.location[0]?.desc}</h2>
-                  <ul class='flex flex-col gap-y-[1rem] justify-center items-center text-center'>
-                    {arrivals?.arrival.map((arrival) => (
-                      <li key={arrival.id}>{arrival.estimated}</li>
-                    ))}
+                  <ul class="flex flex-col gap-y-[1rem] justify-center items-center text-center">
+                    {}
                   </ul>
                 </div>
               );
@@ -57,11 +68,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Transit Arrivals',
+  title: "Transit Arrivals",
   meta: [
     {
-      name: 'description',
-      content: 'Transit arrivals app home page',
+      name: "description",
+      content: "Transit arrivals app home page",
     },
   ],
 };
